e32046bbcfe326551e75c69afcf4134f1ea5aaa4
BobbyPlitsch <robertpeitsch@gmx.net>
2017-01-12 11:14:45 +0100

    4.2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,7 +29,7 @@ class ProductsController < ActionController::Base
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
+        format.json { render :show, status: :create, location: @product }
       else
         format.html { render :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
