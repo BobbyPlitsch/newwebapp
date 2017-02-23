6761b78aae113adaaac361fec80d0183eaeac5d4
BobbyPlitsch <robertpeitsch@gmx.net>
2017-02-23 11:35:20 +0100

    rating pics

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -5,17 +5,5 @@ $(document).on('turbolinks:load', function(){
       score: function() {
        return $(this).attr('data-score');
       }
-    });
-});
-
-
-$(document).on('ready page:load', function(){
-  $('.img-zoom').elevateZoom();
-  $('.rating').raty( { path: '/assets/raty', scoreName: 'comment[rating]' });
-  $('.rated').raty({ path: '/assets/raty',
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
+   });
 });
