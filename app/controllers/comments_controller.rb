08c39784e6e55ab579e5c851ecfa01366299dc70
BobbyPlitsch <robertpeitsch@gmx.net>
2017-02-23 16:32:04 +0100

    5.9 Validation and Pagination

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,23 @@
 class CommentsController < ApplicationController
 
 
- def create
-   @product = Product.find(params[:product_id])
-   @comments = @product.comments.new(comment_params)
-   @comments.user = current_user
-   @comments.save
-   redirect_to product_path(@product)
+ def index
+   @comments = Comment.paginate(:page => params[:page], :per_page => 5)
  end
+ def create
+     @product = Product.find(params[:product_id])
+     @comment = @product.comments.new(comment_params)
+     @comment.user = current_user
+     respond_to do |format|
+       if @comment.save
+         format.html { redirect_to @product, notice: 'Review was created successfully.' }
+         format.json { render :show, status: :created, location: @product }
+       else
+         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+         format.json { render json: @comment.errors, status: :unprocessable_entity }
+       end
+     end
+   end
 
  def destroy
  end
