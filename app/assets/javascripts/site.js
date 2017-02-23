bcc42876517657b820d8c5367c3ae0f1f6d80e5f
BobbyPlitsch <robertpeitsch@gmx.net>
2017-02-23 11:31:34 +0100

    rating picture failure

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,21 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty({ path: "/assets", scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
+    $('.rating').raty({ path: "/assets/raty", scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets/raty',
       readOnly: true,
       score: function() {
        return $(this).attr('data-score');
       }
     });
+});
+
+
+$(document).on('ready page:load', function(){
+  $('.img-zoom').elevateZoom();
+  $('.rating').raty( { path: '/assets/raty', scoreName: 'comment[rating]' });
+  $('.rated').raty({ path: '/assets/raty',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
 });
