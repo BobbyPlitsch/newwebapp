bb2260c965c0cbe6e0c6174392aca2bedd65096a
BobbyPlitsch <robertpeitsch@gmx.net>
2017-02-27 19:03:32 +0100

    migration price

--- db/migrate/20170226183907_change_price_in_products.rb
+++ db/migrate/20170226183907_change_price_in_products.rb
@@ -1,5 +1,5 @@
 class ChangePriceInProducts < ActiveRecord::Migration[5.0]
   def change
-    change_column :products, :price, :decimal
+    change_column :products, :price, 'decimal USING CAST(price AS decimal)'
   end
 end
