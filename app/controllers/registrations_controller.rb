860ec126631662880a02042c9cb7e85174b50a86
BobbyPlitsch <robertpeitsch@gmx.net>
2017-03-01 15:26:58 +0100

    6.1 removed commented code from the registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, :only => [:create]
-  # before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # protected
-  #
-  #   def configure_permitted_parameters
-  #     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
-  #   end
+
   protected
 
   def configure_permitted_parameters
