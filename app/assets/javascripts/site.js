e9f23a976211e837c450c0bdb439a8a7cd2154aa
BobbyPlitsch <robertpeitsch@gmx.net>
2017-05-14 19:48:53 +0200

    adding ajax for comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function(){
     $('.rating').raty({ path: "/assets", scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
+        readOnly: true,
       score: function() {
        return $(this).attr('data-score');
       }
