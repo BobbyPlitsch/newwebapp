ab0d11afccbf06fe4834d72812c8bfae402faac3
BobbyPlitsch <robertpeitsch@gmx.net>
2017-05-21 22:41:21 +0200

    Adding Action Cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://bobbyplitsch.herokuapp.com//cable"
+config.action_cable.allowed_request_origins = ['https://bobbyplitsch.herokuapp.com/', 'http://bobbyplitsch.herokuapp.com/']
 end
